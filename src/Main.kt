39de2affbef1fedb476879a7c3385a79c7fee476
spearkkk <spearkkp@gmail.com>
2021-01-30 11:19:35 +0900

    Clear 13
    https://leetcode.com/problems/roman-to-integer/

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,9 @@
-import palindrome_number_9.Solution
+import roman_to_integer_13.Solution
 
 fun main() {
-    val testSet: List<Int> = listOf(121, -121, 10, -101, 123, 101, -123, 10)
+    val testSet: List<String> = listOf("III", "IV", "IX", "LVIII", "MCMXCIV")
 
     for (test in testSet) {
-        println(Solution().isPalindrome(test))
+        println(Solution().romanToInt(test))
     }
-
 }
---
+++ src/roman_to_integer_13/Solution.kt
@@ -0,0 +1,30 @@
+package roman_to_integer_13
+
+// https://leetcode.com/problems/roman-to-integer/
+class Solution {
+    fun romanToInt(s: String): Int {
+        var one = 1
+        var five = 5
+        var ten = 10
+        var fiveTen = 50
+        var tenTen = 100
+        var fiveTenTen = 500
+        val tenTenTen = 1000
+
+        var sum = 0
+
+        for (elem in s.reversed()) {
+            when (elem) {
+                'I' -> sum += one
+                'V' -> { sum += five; one *= -1 }
+                'X' -> { sum += ten; one *= -1; five *= -1 }
+                'L' -> { sum += fiveTen; one *= -1; five *= -1; ten *= -1 }
+                'C' -> { sum += tenTen; one *= -1; five *= -1; ten *= -1; fiveTen *= -1 }
+                'D' -> { sum += fiveTenTen; one *= -1; five *= -1; ten *= -1; fiveTen *= -1; tenTen *= -1 }
+                'M' -> { sum += tenTenTen; one *= -1; five *= -1; ten *= -1; fiveTen *= -1; tenTen *= -1; fiveTenTen *= -1 }
+            }
+        }
+
+        return sum
+    }
+}
