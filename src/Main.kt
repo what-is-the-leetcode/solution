9045e7516f7f1a678a68fafb08d55aef20d083a8
spearkkk <spearkkp@gmail.com>
2021-02-04 08:44:29 +0900

    Clear 27

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,11 @@
-import remove_duplicates_from_sorted_array_26.Solution
+import remove_element_27.Solution
 
 fun main() {
-    val test: IntArray = intArrayOf(-1, 1, 1, 2)
+    val testSet1: List<IntArray> = listOf(intArrayOf(3, 2, 2, 3), intArrayOf(0, 1, 2, 2, 3, 0, 4, 2), intArrayOf())
+    val testSet2: List<Int> = listOf(3, 2, 0)
 
-    println(Solution().removeDuplicates(test))
-    println(test.contentToString())
+    for ((i, `val`) in testSet2.withIndex()) {
+        println(Solution().removeElement(testSet1[i], `val`))
+        println(testSet1[i].contentToString())
+    }
 }
---
+++ src/remove_element_27/Solution.kt
@@ -0,0 +1,26 @@
+package remove_element_27
+
+/**
+ * 현재 위치의 값이 주어진 값인 `val`과 같다면 유요한 위치(limit)의 마지막 값으로 변경한다. 단, 현재 위치의 index 값은 변하지 않음.
+ * 그리고 limit 를 감소시킨다.
+ *
+ * `val`과 다르다면 현재 위치의 index 를 증가 시킨다.
+ */
+class Solution {
+    fun removeElement(nums: IntArray, `val`: Int): Int {
+        var cur = 0
+        var limit = nums.size
+
+        while (cur < limit) {
+            if (nums[cur] == `val`) {
+                nums[cur] = nums[limit - 1]
+                nums[limit - 1] = -1
+                limit -= 1
+            } else {
+                cur += 1
+            }
+        }
+
+        return limit
+    }
+}
