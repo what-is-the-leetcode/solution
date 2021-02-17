8edd5a02d8754c9baeb2b0a83800ad5eb72b896f
spearkkk <spearkkp@gmail.com>
2021-02-17 23:00:49 +0900

    Clear 118

--- src/Main.kt
+++ src/Main.kt
@@ -1,19 +1,5 @@
-import algorithm.BinaryTreeLevelOrderTraversalII
-import algorithm.MergeSortedArray
-import algorithm.TreeNode
+import algorithm.PascalsTriangle
 
 fun main() {
-    val t1 = TreeNode(15)
-    val t2 = TreeNode(7)
-
-    val t3 = TreeNode(9)
-    val t4 = TreeNode(20)
-    t4.left = t1
-    t4.right = t2
-
-    val root = TreeNode(3)
-    root.left = t3
-    root.right = t4
-
-    println(BinaryTreeLevelOrderTraversalII().levelOrderBottom(root))
+    println(PascalsTriangle().generate(5))
 }
---
+++ src/algorithm/PascalsTriangle.kt
@@ -0,0 +1,21 @@
+package algorithm
+
+class PascalsTriangle {
+    fun generate(numRows: Int): List<List<Int>> {
+        if (numRows == 0) {
+            return listOf()
+        }
+
+        val holder: MutableList<List<Int>> = mutableListOf(listOf(1))
+
+        for (i in 2 .. numRows) {
+            val tmp: MutableList<Int> = mutableListOf(holder[i - 1 - 1][0])
+            for (j in 1 until i - 1) {
+                tmp.add(holder[i - 1 - 1][j - 1] + holder[i - 1 - 1][j])
+            }
+            tmp.add(holder[i - 1 - 1][i - 2])
+            holder.add(tmp)
+        }
+        return holder
+    }
+}
